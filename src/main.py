399be9d06cfc6eeef87ae5b56dcb181daa3ada4b
TakersJP <97802658+TakersJP@users.noreply.github.com>
2025-10-19 19:26:15 -0700

    updated line 2 and 3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
-from core.database import init_db
-from core.config_manager import save_config, load_config
+from src.core.database import init_db
+from src.core.config_manager import save_config, load_config
 # This is for testing if your local environment is running the "virtual environment"
 # It should say True
 def check_virtual_env():
@@ -8,7 +8,7 @@ def check_virtual_env():
 
 if __name__ == "__main__":
 #if __name__ == "main":
-    print("Running in virtual env:", check_virtual_env())
+    #print("Running in virtual env:", check_virtual_env())
     init_db()
     config_data = {"theme": "dark", "notifications": True}
     save_config(config_data)
