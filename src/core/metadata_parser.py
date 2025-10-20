472cfae85085c3b4f02a26c593de92323fb34292
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-10-19 17:54:40 -0700

    feat(metadata_parser): add basic setup for `parse_metadata`

---
+++ src/core/metadata_parser.py
@@ -0,0 +1,13 @@
+def parse_metadata(folder_path: str = ""):
+    """
+    Opens up a folder from a recently extracted zip file and lists the file type and
+    timestamp on file modifications
+
+    Keyword arguments:
+    folder_path -- the path to the directory to be parsed (default "")
+    """
+    test_directory = r"C:\Users\anilo\Desktop\test-directory-capstone"
+    print(test_directory)
+
+
+parse_metadata()
