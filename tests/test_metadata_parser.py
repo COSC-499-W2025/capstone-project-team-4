ef396cd691301b8a551c6a8b939bbda4270abe3b
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-10-23 15:37:41 -0700

    test(metadata_parser): add first test for parsing metadata

---
+++ tests/test_metadata_parser.py
@@ -0,0 +1,23 @@
+import pandas as po
+
+from src.core.metadata_parser import parse_metadata
+
+
+def test_parse_metadata_returns_dataframe(temp_path):
+    # Create a test directory and files
+    directory = temp_path + "/data"
+    directory.mkdir()
+    file_1 = (directory + "/file1.txt").write_text("Chicken time!")
+    file_2 = (directory + "/file2.log").write_text("Chicken log!")
+
+    dataframe = parse_metadata(directory)
+
+    # Check type
+    assert isinstance(dataframe, po.DataFrame)
+
+    # Check if dataframe has both files
+    assert len(dataframe) == 2
+
+    # Check if the columns exist
+    expected_columns = {"filename", "path", "file_type", "last_modified"}
+    assert expected_columns.issubset(dataframe.columns)
