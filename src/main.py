26ff2369981a12eedf41e81c00e6d57afdb7f1c8
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-10-26 21:23:22 -0700

    fix(main): give proper naming scheme for `__main__`

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import sys
+
+
 # This is for testing if your local environment is running the "virtual environment"
 # It should say True
 def check_virtual_env():
     return sys.prefix != sys.base_prefix
 
-if __name__ == "main":
-        print("Running in virtual env:", check_virtual_env())
+
+if __name__ == "__main__":
+    print("Running in virtual env:", check_virtual_env())
