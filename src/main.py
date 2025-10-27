f6bdc683a26af7d8fdf61d9d4013889d8aba1be0
alepiman <aliffmlg@gmail.com>
2025-10-26 18:00:44 -0700

    fix(main): correct the main module check condition for proper execution

--- src/main.py
+++ src/main.py
@@ -4,5 +4,5 @@ import sys
 def check_virtual_env():
     return sys.prefix != sys.base_prefix
 
-if __name__ == "main":
-        print("Running in virtual env:", check_virtual_env())
+if __name__ == "__main__":
+    print("Running in virtual env:", check_virtual_env())
