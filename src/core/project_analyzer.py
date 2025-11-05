41a5ad24dd56e31a61e484476a9ddebae0a3c4a7
AnLaxina <anilovlaxina@gmail.com>
2025-11-05 15:35:41 -0800

    feat(project_analyzer): add working list for contributors

--- src/core/project_analyzer.py
+++ src/core/project_analyzer.py
@@ -17,16 +17,30 @@ def analyze_contributors(project_path = "."):
     """
     repo = Repo(project_path)
     author_commits = defaultdict(int)
+    author_info = {}
 
     # Count the total commits for the repo
     for commit in repo.iter_commits():
-        author_commits[commit.author.name] += 1
+        email = commit.author.email.lower()
+        name = commit.author.name
+
+        # Skip bots, cause who cares about them
+        if "[bot]" in name.lower():
+            continue
+
+        author_commits[email] += 1
+        author_info[email] = name
+    
     total = sum(author_commits.values())
 
-    print(f"The total is: ${total}")
-    print(author_commits)
+    # Right now, this prints the amount of collaborators twice. Asin, it prints the `.noreply.github.com`, and the
+    # author's actual email. For functionality and data, we could just keep this as is.
+    contributors = [
+        {"name": author_info[email], "email":email, "commits": num_of_commits, "percent": round((num_of_commits / total) * 100, 2)}
+        for email, num_of_commits in author_commits.items()
+    ]
 
-    
+    print(contributors)
 
 
 if __name__ == "__main__":
