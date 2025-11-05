d1f9004b744f84745c76107a9f0fca2f181ddcd3
AnLaxina <anilovlaxina@gmail.com>
2025-11-05 14:44:18 -0800

    feat(project_analyzer): add basic template for `analyze_contributors`

---
+++ src/core/project_analyzer.py
@@ -0,0 +1,16 @@
+def analyze_contributors(project_path = ".") {
+    """
+    Opens up the project folder that analyzes the contributors on a project
+    Looks at the amount of contributors and returns an object
+
+    Keyword arguments:
+    project_path -- the path to the root project (default "." or current directory)
+
+    Returns:
+    A dictionary/hash map of contributors each having their name, commits, and the amount of work committed (percentage)
+    """
+}
+
+
+if __name__ == "__main__":
+    print("It's run on here!")
