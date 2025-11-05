baf314949f3999771f814ba63f5ae51ba98a5415
AnLaxina <anilovlaxina@gmail.com>
2025-11-05 15:13:29 -0800

    feat(project_analyzer): add basic functionality in getting total commits

--- src/core/project_analyzer.py
+++ src/core/project_analyzer.py
@@ -1,4 +1,10 @@
-def analyze_contributors(project_path = ".") {
+from git import Repo
+# Use defaultdict instead of a regular dict to initialize default keys automatically 
+from collections import defaultdict
+
+import os
+
+def analyze_contributors(project_path = "."):
     """
     Opens up the project folder that analyzes the contributors on a project
     Looks at the amount of contributors and returns an object
@@ -9,8 +15,21 @@ def analyze_contributors(project_path = ".") {
     Returns:
     A dictionary/hash map of contributors each having their name, commits, and the amount of work committed (percentage)
     """
-}
+    repo = Repo(project_path)
+    author_commits = defaultdict(int)
+
+    # Count the total commits for the repo
+    for commit in repo.iter_commits():
+        author_commits[commit.author.name] += 1
+    total = sum(author_commits.values())
+
+    print(f"The total is: ${total}")
+    print(author_commits)
+
+    
 
 
 if __name__ == "__main__":
-    print("It's run on here!")
+    # For testing, just use the current working directory
+    working_directory = os.getcwd()
+    analyze_contributors(working_directory);
