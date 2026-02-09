adf48dd6cdd1cd018bb1118a2cce9c05ca867345
AnLaxina <anilovlaxina@gmail.com>
2026-02-08 18:48:01 -0800

    fix: remove `str` casting

--- backend/src/api/dependencies.py
+++ backend/src/api/dependencies.py
@@ -38,7 +38,7 @@ async def get_current_user(
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-        user_id = str(payload.get("sub"))
+        user_id = payload.get("sub")
         if user_id is None:
             raise credentials_exception
     except JWTError:
