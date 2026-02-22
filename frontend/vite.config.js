9b46edd88ab16fc918ace83b487683f94dc3bc6b
AnLaxina <anilovlaxina@gmail.com>
2026-02-22 12:01:18 -0800

    fix(frontend): add `allowedHosts` for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,24 +5,25 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(import.meta.dirname, "./src"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
-    server: {
-        host: '0.0.0.0',
-        proxy: {
-            '/api': {
-                target: process.env.VITE_API_URL || 'http://localhost:8000',
-                changeOrigin: true,
-            }
-        }
+  },
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["capstoneteam4.com", "test-frontend.capstoneteam4.com"],
+    proxy: {
+      "/api": {
+        target: import.meta.env.VITE_API_URL || "http://localhost:8000",
+        changeOrigin: true,
+      },
     },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/tests/setup.js',
-    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.js",
+  },
 });
