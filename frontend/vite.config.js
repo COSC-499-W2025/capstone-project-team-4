13c04daafa2cfb8d67105f678314f12619d427df
AnLaxina <anilovlaxina@gmail.com>
2026-02-22 17:30:32 -0800

    fix: add proper `.env` to the vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,34 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import process from "process";
+// We need loadEnv as we can't use import.meta.env.{whatever} until after this config is loaded
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-    allowedHosts: ["capstoneteam4.com", "test-frontend.capstoneteam4.com"],
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL || "http://localhost:8000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(import.meta.dirname, "./src"),
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/tests/setup.js",
-  },
+    server: {
+      host: "0.0.0.0",
+      allowedHosts: ["capstoneteam4.com", "test-frontend.capstoneteam4.com"],
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8000",
+          changeOrigin: true,
+        },
+      },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/tests/setup.js",
+    },
+  };
 });
