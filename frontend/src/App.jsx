f7fb9fee81532e4b862591f1424d689d317d6a7f
Kiichiro-suganuma0209 <kiichiro.suganuma0209@gmail.com>
2026-03-08 14:26:08 -0700

    account page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,47 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Main from '@/pages/Instruction';
-import Generator from '@/pages/Generator';
+import Main from "@/pages/Instruction";
+import Generator from "@/pages/Generator";
+import AccountPage from "@/pages/Account";
 import LoginPage from "@/pages/auth/login";
 import SignupPage from "@/pages/auth/signup";
 import { isAuthenticated } from "@/lib/auth";
 import "./App.css";
 
 function ProtectedRoute({ children }) {
-    if (!isAuthenticated()) {
-        return <Navigate to="/login" replace />;
-    }
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" replace />;
+  }
 
-    return children;
+  return children;
 }
 
 function App() {
-    return (
-        <main>
-        <Routes>
-            <Route path="/" element={<Main />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/signup" element={<SignupPage />} />
-                        <Route
-                            path="/generate"
-                            element={
-                                <ProtectedRoute>
-                                    <Generator />
-                                </ProtectedRoute>
-                            }
-                        />
-        </Routes>
-        </main>
-    );
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/generate"
+          element={
+            <ProtectedRoute>
+              <Generator />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <ProtectedRoute>
+              <AccountPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
